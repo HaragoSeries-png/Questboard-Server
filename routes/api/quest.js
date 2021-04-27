6a0b30b3b5bb5a5bb751ea550e876776a3ee4b89
HaragoSeries-png <62962342+HaragoSeries-png@users.noreply.github.com>
2021-04-27 15:23:23 +0700

    Update quest.js

--- routes/api/quest.js
+++ routes/api/quest.js
@@ -16,11 +16,11 @@ Qc.on('change',change=> {
   Ql =  ql
 })
 
-cloudinary.config({ 
-  cloud_name: process.env.CLOUD_NAME, 
-  api_key: process.env.CLOUD_KEY,
-  api_secret: process.env.CLOUD_SECRET
-});
+// cloudinary.config({ 
+//   cloud_name: process.env.CLOUD_NAME, 
+//   api_key: process.env.CLOUD_KEY,
+//   api_secret: process.env.CLOUD_SECRET
+// });
 
 
 
